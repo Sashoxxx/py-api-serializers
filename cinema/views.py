1426018e99a76d27a30033130636626866269078
Oleksandr Pivtorak <Sanjok150397@gmail.com>
2025-09-03 18:47:47 +0300

    fix

--- cinema/views.py
+++ cinema/views.py
@@ -1,4 +1,6 @@
+from django.db.models import QuerySet
 from rest_framework import viewsets
+from rest_framework.serializers import Serializer
 
 from cinema.models import Actor, Genre, CinemaHall, Movie, MovieSession
 from cinema.serializers import (
@@ -31,7 +33,7 @@ class CinemaHallViewSet(viewsets.ModelViewSet):
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.prefetch_related("genres", "actors")
 
-    def get_serializer_class(self):
+    def get_serializer_class(self) -> type[Serializer]:
         if self.action == "list":
             return MovieListSerializer
         if self.action == "retrieve":
@@ -42,9 +44,16 @@ class MovieViewSet(viewsets.ModelViewSet):
 class MovieSessionViewSet(viewsets.ModelViewSet):
     queryset = MovieSession.objects.select_related("movie", "cinema_hall")
 
-    def get_serializer_class(self):
+    def get_serializer_class(self) -> type[Serializer]:
         if self.action == "list":
             return MovieSessionListSerializer
         if self.action == "retrieve":
             return MovieSessionDetailSerializer
         return MovieSessionSerializer
+
+    def get_queryset(self) -> QuerySet:
+        if self.action == "retrieve":
+            return self.queryset.prefetch_related(
+                "movie__genres", "movie__actors"
+            )
+        return self.queryset
